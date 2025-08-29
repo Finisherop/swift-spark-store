0c0ec3dab935595e4cf4d70889dfd67bd0af5276
Finisherop <160778489+Finisherop@users.noreply.github.com>
2025-08-29 18:59:48 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,13 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Service Worker Register
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("✅ Service Worker registered:", reg))
+      .catch((err) => console.log("❌ Service Worker registration failed:", err));
+  });
+}
