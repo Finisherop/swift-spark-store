94c9bdf0a67a5dd79df9a86d4ad1e0551ecd23de
Finisherop <160778489+Finisherop@users.noreply.github.com>
2025-08-30 08:11:56 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import "../styles/globals.css";
+import "../globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
